4bbd2187d32a47edd1ba3b8942a01ec1c65652b9
Pedro Herrera <pedro.herrera@softtek.com>
2017-08-02 10:33:32 -0700

    Adding a Security group

--- main.tf
+++ main.tf
@@ -3,9 +3,28 @@ provider "aws"{
 }
 
 resource "aws_instance" "terraform"{
-	ami="ami-327f5352"
+	ami="ami-09d2fb69"
 	instance_type="t2.micro"
+	vpc_security_group_ids=["${aws_security_group.webserver.id}"]
+	user_data=<<-EOF
+			#!/bin/bash
+			echo "Hello world" > index.html
+			nohub busybox httpd -f -p 8080 &
+		EOF
 	tags{
 		Name="Terraform-Lab"
 	}
+}
+
+resource "aws_security_group" "webserver"{
+
+	name="Terraform-Web-Server"
+
+	ingress{
+		from_port=8080
+		to_port=8080
+		protocol="tcp"
+		cidr_blocks=["0.0.0.0/0"]
+	}
+
 }
